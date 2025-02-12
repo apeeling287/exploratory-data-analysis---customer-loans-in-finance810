32a91b2c40b9f053d6147ad1393783d39ea5fac1
Your Name <annabel.peel@gmail.com>
2025-02-12 11:13:21 +0000

    created separate function to export dataframe to csv

--- db_utils.py
+++ db_utils.py
@@ -1,15 +1,23 @@
 import yaml
 from sqlalchemy import create_engine
-from sqlalchemy import text
 import pandas as pd
 
 
 def load_yaml(yaml_file):
-    '''loads RDS credentials as a dict'''
+    '''
+    loads RDS credentials as a dict from credentials.yaml file
+    '''
     with open(yaml_file, "r") as f:
         credentials_dict = yaml.load(f, Loader=yaml.SafeLoader)
     print(credentials_dict)
     return credentials_dict
+
+def df_to_csv(df):
+    '''
+    converts dataframe to csv file
+    '''
+    df.to_csv("loan_payments.csv")
+    print("exporting to csv")
 
 
 class RDSDatabaseConnector:
@@ -18,30 +26,26 @@ class RDSDatabaseConnector:
     '''
     def __init__(self, my_dict):
         self.my_dict = my_dict  
-        self.engine = None
 
     def SQL_alchemy_connection(self):
-        #initialise SQLAlchemy engine 
+        '''
+        initialise SQLAlchemy engine 
+        '''
         self.engine = create_engine(f"{self.my_dict['DATABASE_TYPE']}+{self.my_dict['DBAPI']}://{self.my_dict['RDS_USER']}:{self.my_dict['RDS_PASSWORD']}@{self.my_dict['RDS_HOST']}:{self.my_dict['RDS_PORT']}/{self.my_dict['RDS_DATABASE']}")
         print("Database connected")
 
-    def extract_data(self):
-        #extracts data and returns it as a pandas dataframe 
-        # with self.engine.execution_options(isolation_level="AUTOCOMMIT").connect() as conn:
-        #     loan_payments = pd.read_sql_table("loan_payments", self.engine)
-        #     loan_payments.head()
-        # with self.engine.connect() as conn:
-        #     result = conn.execute(text("select 'hello world'"))
-        #     print(result.all())
+    def extract_data(self, sql_table):
+        '''
+        extract data from database and return it as a dataframe
+        '''
         with self.engine.connect() as conn:
-            df = pd.read_sql_table('loan_payments', conn)
-            df.head()
-            print(df)
-            df.to_csv('loan_payments.csv')
-
+            df = pd.read_sql_table(sql_table, conn)
+            print(df.head()) 
+            return df
 
 credentials = load_yaml("credentials.yaml")
-
 my_database = RDSDatabaseConnector(credentials)
 my_database.SQL_alchemy_connection()
-my_database.extract_data()
+loan_payments_df = my_database.extract_data('loan_payments')
+
+df_to_csv(loan_payments_df)
