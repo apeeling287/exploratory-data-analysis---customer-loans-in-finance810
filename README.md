52c86300e49ad58b477d8dbe23ffb55c34125e24
Your Name <annabel.peel@gmail.com>
2025-02-19 12:12:50 +0000

    created datatransform class for changing data types and dataframeinfo for gaining information about dataframe

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # Exploratory Data Analysis Project-Customer Loans in Finance
 ## Description
-what project does and why it exists 
 this project is to display my abilities of data analysis using techniques such as object oriented programming, connecting to databases using SQLAlchemy and statistical analysis of data with the example of customer loans in finance 
+# Table of Contents
+1. [Introduction](#introduction)
+2. [Section 1](#section-1)
+    - [Subsection 1.1](#subsection-1.1)
+    - [Subsection 1.2](#subsection-1.2)
+
+## Introduction
+This project uses Object Oriented Programming to connect to a RDS and extract data from this database for data analysis
+
+## Section 1
+Content here...
+
+### Subsection 1.1
+Content here...
+
+### Subsection 1.2
+Content here...
--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,7 @@
 import yaml
 from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
 
 
 def load_yaml(yaml_file):
@@ -20,11 +21,11 @@ def df_to_csv(df):
     print("exporting to csv...")
 
 def csv_to_df(csv):
-    print("importing csv to pandas dataframe")
-    df = pd.read_csv(csv)
+    print("importing csv to pandas dataframe...")
+    df = pd.read_csv(csv, index_col=0)
     df.shape
-    df.info()
-    df.describe()
+    df.info
+    return df
 
 
 class RDSDatabaseConnector:
@@ -50,12 +51,79 @@ class RDSDatabaseConnector:
             print(df.head()) 
             return df
 
-credentials = load_yaml("credentials.yaml")
-my_database = RDSDatabaseConnector(credentials)
-my_database.SQL_alchemy_connection()
-loan_payments_df = my_database.extract_data('loan_payments')
-df_to_csv(loan_payments_df)
-csv_to_df("loan_payments.csv")
+
+class DataTransform:
+    '''
+    handles conversions of columns to different data formats
+    '''
+    def __init__(self, df):
+        self.df = df
+
+    def convert_to_date(self, columns):
+        print("converting columns to date format...")
+        self.df[columns] = self.df[columns].apply(pd.to_datetime, format="%b-%Y", errors='coerce')
+        return self.df
+
+    def convert_to_str(self, columns):
+        print("converting columns to string format...")
+        self.df[columns] = self.df[columns].astype(str)
+        return self.df
+
+    def convert_to_numeric(self, columns):
+        print("converting columns to numeric format...")
+        self.df[columns] = self.df[columns].apply(pd.to_numeric, errors='raise')
+        return self.df
+
+       
+class DataFrameInfo:
+    '''
+    gives information about dataframe: data types, statistical values, 
+    distinct values, shape, count of Nulls 
+    '''
+    def __init__(self, df):
+        self.df = df
+    
+    def describe_df(self):
+        print("column data types: ")
+        return self.df.info()
+
+    def statistical_values(self):
+        print("column statistical values: mean, median, std")
+        return self.df.describe(include="all")
+
+    def unique_values(self):
+        print("unique values per column:")
+        self.df = self.df.nunique().to_frame().reset_index()
+        self.df.columns = ["Variable", "DistinctCount"]
+        return self.df
+    
+    def df_shape(self):
+        print("Shape of the df: ")
+        return self.df.shape
+    
+    def count_of_nulls(self):
+        null_count = self.df.isnull().sum()
+        #print(f"NaN count per column:\n{null_count}" )
+        print("percentage and count of missing values: ")
+        percent_missing = round(self.df.isnull().sum() * 100 / len(self.df), 2)
+        missing_values_df = pd.DataFrame({"percentage_missing":percent_missing,
+                                          "null_count": null_count
+                                          })
+        return missing_values_df
+ 
 
 
+if __name__ == "__main__":
 
+    credentials = load_yaml("credentials.yaml")     #create credentials variable
+    my_database = RDSDatabaseConnector(credentials) #initialise the class
+    my_database.SQL_alchemy_connection()        #call method to connect to database
+    loan_payments_df = my_database.extract_data('loan_payments')  ##call method to extract data from database
+    df_to_csv(loan_payments_df) #convert dataframe to csv 
+    loan_payments_df_v2 = csv_to_df("loan_payments.csv")  #converts local csv file to dataframe
+    transformed_df = DataTransform(loan_payments_df_v2)
+    columns = ["last_payment_date", "next_payment_date", "last_credit_pull_date"]
+    transformed_df.convert_to_date(columns)
+    print(transformed_df)
+    df_info = DataFrameInfo(transformed_df)
+    df_info.describe_df()
